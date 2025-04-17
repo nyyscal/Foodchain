2dfc8d8430023ed86dd043c9b0e10dff96ec739a
Nischal <nyyscal541556@gmail.com>
2025-04-17 15:50:58 +0545

    Hero Section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,59 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) =>{
+  const currency = import.meta.VITE_CURRENCY
   const navigate = useNavigate();
   const [user,setUser] = useState(null)
+  const [products,setProducts] = useState([])
   const [isSeller,setIsSeller] = useState(false)
   const [showUserLogin,setShowUserLogin] = useState(false)
+  const [cartItems,setCartItems] = useState({})
 
-  const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin}
+  const fetchProducts = async()=>{
+    setProducts(dummyProducts)
+  }
+
+  const addToCart = (itemId) =>{
+    let cartData = structuredClone(cartItems)
+    if(cartData[itemId]){
+      cartData[itemId] += 1
+    }else{
+      cartData[itemId] = 1
+    }
+    setCartItems(cartData)
+    toast.success("Added to Cart!")
+  }
+
+  const updateCartItem = (itemId, quantity)=>{
+    let cartData = structuredClone(cartItems)
+    cartData[itemId] = quantity;
+    setCartItems(cartData)
+    toast.success("Cart Updated!")
+  }
+
+  const removeCartItem = (itemId) =>{
+    let cartData = structuredClone(cartItems)
+    if(cartData[itemId]){
+      cartData[itemId] -= 1;
+      if(cartData[itemId] ===0){
+        delete cartData[itemId]
+      }
+    }
+    toast.success("Removed from Cart!")
+    setCartItems(cartData)
+  }
+
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+  const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin, products,currency,addToCart,updateCartItem,removeCartItem, cartItems}
 
   return <AppContext.Provider value={value}>
     {children}
